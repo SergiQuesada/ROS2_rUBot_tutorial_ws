704c24ab22370608d9977d602332e64e39ad2417
mariabaro <mbarotar7@alumnes.ub.edu>
2025-11-02 17:45:16 +0000

    move_turtle updated

--- src/ros2_move_turtle/setup.py
+++ src/ros2_move_turtle/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
     'console_scripts': [
-        'move_turtle_exec = ros2_move_turtle.move_turtle:main',
+        'move_turtle = ros2_move_turtle.move_turtle:main',
     ],
     },
 )
